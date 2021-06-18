cea22b9312346394db1f2da5625497362f439fab
Sweekrithi  S  Shetty <shettysweekrithi03@gmail.com>
2021-06-18 10:55:13 -0400

    Update Easy : Reverse String.py

--- Easy : Reverse String.py
+++ Easy : Reverse String.py
@@ -1,3 +1,10 @@
+#Write a function that reverses a string. The input string is given as an array of characters s.
+
+ #Example 1:
+
+#Input: s = ["h","e","l","l","o"]
+#Output: ["o","l","l","e","h"]
+
 class Solution:
     def reverseString(self, s):
         left = 0;
